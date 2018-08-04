5d8a05d375eac73c447261471504636cb3353a48
roger21 <roger21@free.fr>
2018-08-04 11:22:56 +0200

    sécurisation du webhook et message de maj

--- git2hfr.php
+++ git2hfr.php
@@ -1,4 +1,7 @@
-<?php /*
+<?php
+
+/*
+
 
 Copyright 2018 XaTriX
 
@@ -15,15 +18,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 
-Script: git2hfr.php
-Purpose: auto-update hfr
+https://github.com/XaaT/hfr-git2hfr/
 
 
-*/ ?><!DOCTYPE html>
+*/
+
+?><!DOCTYPE html>
 <html lang="fr">
 <head>
 <meta charset="utf-8">
-<title>GitHub 2 HFR</title>
+<title>Git 2 HFR</title>
 </head>
 <body>
 <?php
@@ -40,42 +44,78 @@ ini_set("html_errors", "1");
 /* les paramètres */
 /******************/
 
+
 // les messages à actualiser
 
 //   source -> le contenu du message (le raw de github)
 //   formulaire -> la page d'edition du message
+//   (optionnel) message -> le contenu (en BBCode) du message de signalement de la mise à jour
 
 $posts=[
   [
     "source" => "https://raw.githubusercontent.com/XaaT/hfr-toulouse/master/post-1.txt",
     "formulaire" => "https://forum.hardware.fr/message.php?config=hfr.inc&cat=13&post=89952&numreponse=19682010&page=1&p=1&subcat=431&sondage=0&owntopic=2#formulaire",
-    ],
+    "message" => "pouet",
+  ],
   [
     "source" => "https://raw.githubusercontent.com/XaaT/hfr-toulouse/master/post-2.txt",
     "formulaire" => "https://forum.hardware.fr/message.php?config=hfr.inc&cat=13&post=92651&numreponse=23723254&page=1&p=1&subcat=427&sondage=0&owntopic=0#formulaire",
-    ],
-  ];
+    "message" => "",
+  ],
+];
+
 
 // les cookies
-$cookies="md_user=roger21; md_passs=";
+
+$cookies="md_user=roger21; md_passs=8499b50efcb388884c5c45cad94fd53f";
+
+
+// la clé de sécurisation (le content-type du webkook doit être application/json)
+
+$key="Q3FZ7LrTzLvVKp4pfyRIPlBZoZicERU7NopZr1IXhVHKeg7G1glLUoLsEuQBSE7z2McgOdQxfco1RMyHXnFaVPqQqfctiYdcnhsQgNwb7FWUoEatkz6tFmbdmnwM2U4a0T3hU6wlPgZs0je5iE634LrbtR8o3uLdfFWlQIGBU2LaChbZCogtronilySJq6cR";
 
 
 
-/********************************************/
-/* début de la boucle sur tous les messages */
-/********************************************/
-
-foreach($posts as $post){
-
-  $source=$post["source"];
-  $formulaire=$post["formulaire"];
+/***********************************************/
+/* fonction de vérification de la sécurisation */
+/***********************************************/
 
 
+function secured(){
+  global $key, $talktome;
+  if(!$key){
+    $talktome.="<br>\npas de clé de sécurisation<br>\n<br>\n";
+    return true;
+  }
+  if(!isset($_SERVER["HTTP_X_HUB_SIGNATURE"]) ||
+     strpos($_SERVER["HTTP_X_HUB_SIGNATURE"], "sha1=") !== 0){
+    $talktome.="<br>\npas de signature<br>\n<br>\n";
+    return false;
+  }
+  list($algo, $signature)=explode("=", $_SERVER["HTTP_X_HUB_SIGNATURE"]);
+  $content=file_get_contents("php://input");
+  $hash=hash_hmac($algo, $content, $key);
+  if(hash_equals($signature, $hash)){
+    $talktome.="<br>\nsécurisation ok<br>\n<br>\n";
+    return true;
+  }else{
+    $talktome.="<br>\nsécurisation caca<br>\n<br>\n";
+    return false;
+  }
+}
 
-/******************************/
-/* récupération du formulaire */
-/******************************/
 
+
+/***********************/
+/* fonction de postage */
+/***********************/
+
+
+function postage($formulaire, $source, $new=false){
+
+  // récupération du formulaire de postage
+
+  global $cookies, $talktome;
   $context=stream_context_create(["http" => ["method" => "GET", "header" => "Cookie: ".$cookies."\r\n"]]);
   $page=file_get_contents($formulaire, false, $context);
   $dom=new DOMDocument();
@@ -84,11 +124,7 @@ foreach($posts as $post){
   $form=$xpath->query("//form[@name=\"hop\" and @id=\"hop\"]");
   $form=$form->item(0);
 
-
-
-/**************************************************************/
-/* récupération et construction des champs du formaulaire hfr */
-/**************************************************************/
+  // récupération et construction des champs du formaulaire
 
   $data=[];
 
@@ -115,19 +151,52 @@ foreach($posts as $post){
   }
 
   // le message
-  $data["content_form"] = file_get_contents($source);
+  $data["content_form"] = $source;
 
-
-
-/********************/
-/* l'envoie du post */
-/********************/
+  // envoie du formulaire
 
   $url="https://forum.hardware.fr/bdd.php?config=hfr.inc";
+  if($new){
+    $url="https://forum.hardware.fr/bddpost.php?config=hfr.inc";
+  }
   $context=stream_context_create(["http" => ["method" => "POST",
                                              "header" => "Content-type: application/x-www-form-urlencoded\r\nCookie: ".$cookies."\r\n",
                                              "content" => http_build_query($data)]]);
   $result=file_get_contents($url, false, $context);
+
+  // récupération de la réponse
+
+  $dom=new DOMDocument();
+  @$dom->loadHTML($result);
+  $xpath=new DOMXPath($dom);
+  $div=$xpath->query("//div[@class=\"hop\"]/text()");
+  $result=trim($div->item(0)->nodeValue);
+  //$talktome.="<pre>".var_export($data, true)."</pre><br>\n";
+  $talktome.=$result."<br>\n";
+
+}
+
+
+
+/*************************************/
+/* boucle de traitement des messages */
+/*************************************/
+
+
+$talktome="";
+if(secured()){
+  foreach($posts as $post){
+    $source=file_get_contents($post["source"]);
+    $formulaire=$post["formulaire"];
+    postage($formulaire, $source);
+    if(isset($post["message"]) && !empty($post["message"]) && !empty(trim($post["message"]))){
+      $source=trim($post["message"]);
+      $formulaire=preg_replace("/&numreponse=[0-9]+/", "", str_replace("#formulaire", "", $formulaire))."&new=0";
+      //$talktome.="$formulaire<br>\n";
+      postage($formulaire, $source, true);
+    }
+  }
+}
 
 
 
@@ -135,20 +204,10 @@ foreach($posts as $post){
 /* affichage de qqchose pour faire joli */
 /****************************************/
 
-  $dom=new DOMDocument();
-  @$dom->loadHTML($result);
-  $xpath=new DOMXPath($dom);
-  $div=$xpath->query("//div[@class=\"hop\"]/text()");
-  $result=$div->item(0)->nodeValue;
-  echo($result)."<br>";
+
+echo $talktome;
 
 
-
-/******************************************/
-/* fin de la boucle sur tous les messages */
-/******************************************/
-
-}
 
 ?>
 </body>
